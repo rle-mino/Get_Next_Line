a19ed887e03bb1fc7e8d1fc8cde66eef56be4c03
Raphael LE MINOR <rle-mino@e3r3p9.42.fr>
2016-01-07 20:24:24 +0100

    reset2

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rle-mino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/20 12:59:14 by rle-mino          #+#    #+#             */
+/*   Updated: 2016/01/07 20:17:22 by rle-mino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include "libft/includes/libft.h"
+# include <stdlib.h>
+
+# define BUFF_SIZE 100
+
+int					get_next_line(int const fd, char **line);
+
+typedef struct		s_struct
+{
+	char			*buf1;
+	char			*buf2;
+	char			*bin;
+	int				k;
+}					t_struct;
+
+#endif
